8170f422485bd6ecd560dc5d7124e6c3d58b8e50
doorstrawberry <kangminsoo671@gmail.com>
2022-07-04 11:51:56 -0400

    successfully communicated with marvel api

---
+++ src/utilities/marvel-api.js
@@ -0,0 +1,18 @@
+const md5 = require("blueimp-md5");
+
+export async function getMarvelComics(search){
+    let BASE_URL = `https://gateway.marvel.com`
+    let API_KEY = 'c51223db7fb9b72a6ac9b2947734448f'
+    let PRIVATE = `75ac0b3c5d100bb75cb86dc1964102f006b93fc1`
+    let TIMESTAMP = 'sussybaka'
+    let HASH = md5(`${TIMESTAMP}${PRIVATE}${API_KEY}`)
+    let URL = `${BASE_URL}/v1/public/comics?ts=${TIMESTAMP}&apikey=${API_KEY}&hash=${HASH}`
+    const response = await fetch(URL)
+    if (response.ok) {
+        return response.json()
+    }
+    else {
+        throw new Error("Invalid Request")
+    }
+
+}
