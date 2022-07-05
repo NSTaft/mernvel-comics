af1cc652110318ae9c06a24788fe28ded20f07ad
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-05 11:04:00 -0400

    added user usestate to app.js file.

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,27 +1,30 @@
-import './App.css';
-import NavBar from "../../components/NavBar/NavBar.jsx"
-import Header from "../../components/Header/Header.jsx"
+import './App.css'
+import NavBar from '../../components/NavBar/NavBar.jsx'
+import Header from '../../components/Header/Header.jsx'
 
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Account from "../Account/Account.jsx"
-import AuthPage from "../AuthPage/AuthPage.jsx"
-import Collection from "../Collection/Collection.jsx"
-import Library from "../Library/Library.jsx"
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Account from '../Account/Account.jsx'
+import AuthPage from '../AuthPage/AuthPage.jsx'
+import Collection from '../Collection/Collection.jsx'
+import Library from '../Library/Library.jsx'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <NavBar />
-      <Routes>
-        <Route path="/account" element={<Account />} />
-        <Route path="/account/collection" element={<Collection />} />
-        <Route path="/library" element={<Library />} />
-        <Route path="/authentication" element={<AuthPage />}/>
-        <Route path="/*" element={<Navigate to="/authentication" />} />
-      </Routes>
-    </div>
-  );
+	const [user, setUser] = useState(null)
+
+	return (
+		<div className='App'>
+			<Header />
+			<NavBar />
+			<Routes>
+				<Route path='/account' element={<Account />} />
+				<Route path='/account/collection' element={<Collection />} />
+				<Route path='/library' element={<Library />} />
+				<Route path='/authentication' element={<AuthPage />} />
+				<Route path='/*' element={<Navigate to='/authentication' />} />
+			</Routes>
+		</div>
+	)
 }
 
-export default App;
+export default App
