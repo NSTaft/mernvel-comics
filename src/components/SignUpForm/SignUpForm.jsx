eb72c765fade30465226788da614d090bc1a9b86
“GissellValdez” <Gissellvaldez@gmail.com>
2022-07-05 11:51:55 -0400

    in the form tabbed the input properties to make it easier to read

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -34,43 +34,43 @@ export default class SignUpForm extends Component {
                 <form autoComplete="off" onSubmit={this.handleSubmit}>
                     <label>First Name</label>
                     <input
-                    type="text"
-                    name="firstname"
-                    value={this.state.firstname}
-                    onChange={this.handleChange}
-                    required
+                        type="text"
+                        name="firstname"
+                        value={this.state.firstname}
+                        onChange={this.handleChange}
+                        required
                     />
                     <label>Last Name</label>
                     <input
-                    type="text"
-                    name="lastname"
-                    value={this.state.lastname}
-                    onChange={this.handleChange}
-                    required
+                        type="text"
+                        name="lastname"
+                        value={this.state.lastname}
+                        onChange={this.handleChange}
+                        required
                     />
                     <label>Email</label>
                     <input
-                    type="email"
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                    required
+                        type="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        required
                     />
                     <label>Password</label>
                     <input
-                    type="password"
-                    name="password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    required
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        required
                     />
                     <label>Confirm</label>
                     <input
-                    type="password"
-                    name="confirm"
-                    value={this.state.confirm}
-                    onChange={this.handleChange}
-                    required
+                        type="password"
+                        name="confirm"
+                        value={this.state.confirm}
+                        onChange={this.handleChange}
+                        required
                     />
                     <button type="submit" disabled={disable}>
                     SIGN UP
