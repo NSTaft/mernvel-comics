786b71e9e06d7ae806b50c99f171e60fb9636cec
doorstrawberry <kangminsoo671@gmail.com>
2022-07-06 23:41:07 -0400

    page turn works

--- src/pages/Library/Library.jsx
+++ src/pages/Library/Library.jsx
@@ -5,9 +5,11 @@ import "./Library.css"
 
 function Library() {
     const [comicList, setComicList] = useState([])
-    let total = 0
-    let offset = 0
+    const [offset, setOffset] = useState(0)
+    const [firstpage, setFirstPage] = useState(true)
     let limit = 9
+    let total = 0
+    let lastpage = false
 
     async function getMarvelComics(search = "", offset, limit) {
         const comicList = await marvelService.getMarvelComics(search, offset, limit)
@@ -20,11 +22,31 @@ function Library() {
         getMarvelComics("", offset, limit)
     }, [])
 
+    function nextPage() {
+        setOffset(offset + 9)
+        setFirstPage(false)
+        getMarvelComics("", offset + 9, limit)
+    }
+
+    function previousPage(){
+        setOffset(offset - 9)
+        if (offset - 9 === 0){
+            setFirstPage(true)
+        }
+        getMarvelComics("", offset - 9, limit)
+    }
+
     return (
         <>
             <nav className="nav">
-                <li>Previous</li>
-                <li>Next</li>
+                {firstpage ? (
+                    <li onClick={nextPage}>Next</li>
+                ): (
+                    <>
+                        <li onClick={previousPage}>Previous</li>
+                        <li onClick={nextPage}>Next</li>
+                    </>
+                )}
             </nav>
             <div className="library">
                 <div className="grid">
