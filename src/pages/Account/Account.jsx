06ed18375e9ea5a1c1400e50b6b7d9f557ccca00
NSTaft <taftnathan13@gmail.com>
2022-07-07 09:49:02 -0500

    Edit Account Info started.

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react'
+
+
 function Account(props) {
+
+    const [updateUser, setUpdateUser] = useState({ firstname: props.user.firstname, lastname: props.user.lastname, email: props.user.email })
+
+
+    function handleSubmit(evt) {
+        evt.preventDefault()
+        console.log(updateUser)
+    }
+
+    function formUser(evt) {
+        const newFormUser = {[evt.target.name]: evt.target.value}
+        setUpdateUser(newFormUser)
+    }
+
     return (
         <>
-            <h2>FIRST NAME: {props.user.firstname}</h2>
-            <h2>FIRST NAME: {props.user.lastname}</h2>
-            <h2>EMAIL: {props.user.email}</h2>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="firstname" value={props.user.firstname} onChange={formUser}/>
+                    <input type="text" name="lastname" value={props.user.lastname} onChange={formUser}/>
+                    <input type="text" name="email" value={props.user.email} onChange={formUser} />
+                    <button type="submit">EDIT ACCOUNT INFO</button>
+
+                </form>
+            </div>
+
+
         </>
     )
 }
